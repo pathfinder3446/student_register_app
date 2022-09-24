32d6fe91e869f1751f955a9f74e7609977976bf3
Arif <kanebrian0634@gmail.com>
2022-09-24 03:35:58 +0300

    readme added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="info">
-	<p>Click to edit a todo</p>
+
 	<p>Created by <a href="https://github.com/pathfinder3446">Arif Kılınç</a></p>
 	<p>Part of <a href="https://github.com/pathfinder3446/student_register_app">Öğrenci Kayıt Uygulaması</a></p>
 </footer>
