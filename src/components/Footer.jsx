fde300280b7b54a8969f2d3a93f39f62f967f651
pathfinder3446 <kanebrian0634@gmail.com>
2022-12-28 10:34:49 +0300

    ..

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="info">
-
 	<p>Created by <a href="https://github.com/pathfinder3446">Arif Kılınç</a></p>
 	<p>Part of <a href="https://github.com/pathfinder3446/student_register_app">Öğrenci Kayıt Uygulaması</a></p>
 </footer>
